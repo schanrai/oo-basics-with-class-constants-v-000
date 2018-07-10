4c6c3e159d593019ca6e76814c7b881f2143dd2d
Sushi Chanrai <sushi@pathworks.co>
2018-07-10 15:37:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  #attr_reader :brand
 
   BRANDS = []
 
-  #def initialize(brand)
-    #@brand = brand
-  #end
+  def initialize(brand)
+    @brand = brand
+  end
 
   def brand=(brand)
     @brand = brand
