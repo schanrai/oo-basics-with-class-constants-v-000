2aaf3abdb6cb3741db3727e0c1ce0599d0bd8241
Sushi Chanrai <sushi@pathworks.co>
2018-07-10 15:48:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,7 +13,7 @@ class Shoe
   def brand=(brand)
    @brand = brand
    #BRANDS << brand
-   BRANDS << brand unless BRANDS.include?(brand)
+   #BRANDS << brand unless BRANDS.include?(brand)
  end
 
   def cobble
